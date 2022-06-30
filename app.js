956c649683c810eacbff2755dd98d15a5e61ead5
livmeierx <liv.meier13@gmail.com>
2022-06-30 18:28:53 -0400

    created page-template.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const profileDataArgs = process.argv.slice(2);
 
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
+const [name1, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name1, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
